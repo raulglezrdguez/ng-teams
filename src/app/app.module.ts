3fc795484bf5393910c7be0b87ec40a5c61745dc
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-15 17:24:36 -0500

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { TeamsComponent } from './teams/teams.component';
+import { TeamDetailComponent } from './teams/team-detail/team-detail.component';
+import { TeamItemComponent } from './teams/team-list/team-item/team-item.component';
+import { TeamListComponent } from './teams/team-list/team-list.component';
+import { PersonListComponent } from './person-list/person-list.component';
+import { PersonEditComponent } from './person-list/person-edit/person-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TeamsComponent,
+    TeamDetailComponent,
+    TeamListComponent,
+    TeamItemComponent,
+    PersonListComponent,
+    PersonEditComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
