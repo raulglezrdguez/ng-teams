4ccb6824034bc99a005fa8e1467037e6bc8e3cdf
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-18 10:01:34 -0500

    team model created

--- src/app/teams/team-list/team-list.component.ts
+++ src/app/teams/team-list/team-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Team } from '../team.model';
 
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
-  styleUrls: ['./team-list.component.css']
+  styleUrls: ['./team-list.component.css'],
 })
 export class TeamListComponent implements OnInit {
+  teams: Team[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
