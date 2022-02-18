9507895a3f3312c181adfcb84da06bd5951bdb8b
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-18 14:24:35 -0500

    passing parameters between team list and item

--- src/app/teams/team-list/team-item/team-item.component.ts
+++ src/app/teams/team-list/team-item/team-item.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Team } from '../../team.model';
 
 @Component({
   selector: 'app-team-item',
   templateUrl: './team-item.component.html',
-  styleUrls: ['./team-item.component.css']
+  styleUrls: ['./team-item.component.css'],
 })
 export class TeamItemComponent implements OnInit {
+  @Input('team') team: Team = new Team('', '', '');
+  @Output('teamSelected') teamSelected: EventEmitter<Team> =
+    new EventEmitter<Team>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
