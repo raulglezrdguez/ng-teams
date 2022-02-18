ec47a856421cfed22e2cbf9a6f434257637e5868
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-18 14:03:30 -0500

    header navigation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-teams';
+  title = 'Teams Manager';
+  menu = 'teams';
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output('menu') menu: EventEmitter<string> = new EventEmitter<string>();
+  @Input('title') title: string = '';
+
+  setMenu(m: string) {
+    this.menu.emit(m);
+  }
+}
