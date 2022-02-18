67f8988b2a7aa202b966312e0e93d11cd3e3bc5b
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-18 11:27:21 -0500

    person list updated

--- src/app/person-list/person-list.component.ts
+++ src/app/person-list/person-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from '../shared/person.model';
 
 @Component({
   selector: 'app-person-list',
   templateUrl: './person-list.component.html',
-  styleUrls: ['./person-list.component.css']
+  styleUrls: ['./person-list.component.css'],
 })
 export class PersonListComponent implements OnInit {
+  persons: Person[] = [new Person('Joan', 10), new Person('Susan', 20)];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
