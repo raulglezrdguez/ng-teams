8b1060d9a0329d605fca94b8e5a15dad98f3cb7d
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-20 10:32:14 -0500

    person edit form updated

--- src/app/person-list/person-edit/person-edit.component.ts
+++ src/app/person-list/person-edit/person-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Person } from 'src/app/shared/person.model';
 import { PersonService } from '../person.service';
 
@@ -8,20 +9,43 @@ import { PersonService } from '../person.service';
   styleUrls: ['./person-edit.component.css'],
 })
 export class PersonEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef?: ElementRef;
-  @ViewChild('workTimeInput') workTimeInputRef?: ElementRef;
+  // @ViewChild('nameInput') nameInputRef?: ElementRef;
+  // @ViewChild('workTimeInput') workTimeInputRef?: ElementRef;
+  @ViewChild('form') formRef?: NgForm;
+
+  defaultName = 'Frontend';
 
   constructor(private personService: PersonService) {}
 
   ngOnInit(): void {}
 
-  addPerson() {
-    const name = this.nameInputRef?.nativeElement.value;
-    const workTime = +this.workTimeInputRef?.nativeElement.value;
-    this.personService.addPerson(new Person(name, workTime));
-  }
-
   clearList() {
     this.personService.clearList();
+  }
+
+  // onSubmit(form: NgForm) {
+  // onSubmit() {
+  //   const name = this.nameInputRef?.nativeElement.value;
+  //   const workTime = +this.workTimeInputRef?.nativeElement.value;
+  //   this.personService.addPerson(new Person(name, workTime));
+  // }
+  onSubmit() {
+    const name = this.formRef?.value.name;
+    const workTime = +this.formRef?.value.workTime;
+    this.personService.addPerson(new Person(name, workTime));
+    console.log(this.formRef);
+    // override all form values
+    // this.formRef?.setValue({
+    //   name: this.defaultName,
+    //   workTime: 20,
+    // });
+    // patch some form values
+    this.formRef?.form.patchValue({
+      name: this.defaultName,
+    });
+    // reset the form values
+    // this.formRef?.reset({
+    //   name: this.defaultName,
+    // });
   }
 }
